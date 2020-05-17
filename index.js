32d52bc0c2859827496297322d947cf321b4d2bc
Pinky Gautam <57994731+ppkgtmm@users.noreply.github.com>
2020-05-17 15:33:14 +0700

    factorized + read sync

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const optionScraper = require('./scrapeOptions');
 const utils = require('./utils');
 const cheerio = require('cheerio');
 var readline = require('readline');
+var readlineSync = require('readline-sync');
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -22,7 +23,35 @@ async function scrapeHero(hero){
     return hero;
 }
 
-    
+function getChoice(){
+    console.log('Available heroes to scrape');
+    heroes.forEach(hero => console.log(`${hero.id} ${hero.name}`));
+    let choice = readlineSync.question('Which hero(es) would you like to scrape? (enter -1 to exit)"\n');
+    choice = parseInt(choice,10);
+    return choice;
+}
+
+function validateChoice(choice) {
+    if(!isNaN(choice))
+                {
+                    if(choice===-1)
+                    {
+                        return choice;
+                    }
+                    else if(choice<1 || choice>heroes.length)
+                    {
+                        console.log('You have entered invalid integer');
+                        return undefined;
+                    }
+                    else{
+                        return choice;
+                    }
+                }
+                else{
+                    console.log('Please enter an integer');
+                    return undefined;
+                }
+}
 async function main()
 {
     heroes = await getOptions();
@@ -33,16 +62,25 @@ async function main()
             await statsScraper.getStats();
         }
         else if(choice.toLocaleLowerCase().startsWith('B'.toLocaleLowerCase())){
-            console.log('Which hero(es) would you like to scrape?');
-            heroes.forEach(hero => console.log(`${hero.id} ${hero.name}`));
+            while(true)
+            {
+                let choice = getChoice();
+                choice = validateChoice(choice);
+                if(choice!==undefined)
+                {
+                    console.log('choice');
+                }
+                readlineSync.keyIn('Press any key to continue...');
+            }
         }
         else{
             console.log('Invalid option');
+            rl.close();
             process.exit(1);
         }
+        rl.close();
         process.exit(0);
     })
     
 }
 main();
-// statsScraper.getStats();
